623f78ca3d84145b29459be91c7e6c1a78f6d9ff
Graham Kirwan <graham.kirwan4@gmail.com>
2022-06-19 19:05:04 -0400

    artist layout

--- components/layout/PaintingInfo.js
+++ components/layout/PaintingInfo.js
@@ -7,13 +7,23 @@ export default function PaintingInfo({paintingData}) {
   console.log(paintingData)
   return (
     <div className={styles.container}>
-      <div style={{width: '30rem', height: '37rem', position: 'relative'}}>
-        <Image src={"/"+paintingData.images.gallery} layout='fill' objectFit='cover' />
+
+
+    
+      <div className={styles.leftContainer}>
+        <div style={{width: '30rem', height: '37rem', position: 'relative'}}>
+          <Image src={"/"+paintingData.images.gallery} layout='fill' objectFit='cover' />
+        </div>
+        <div className={styles.artistInfo}>
+          <h1>{paintingData.name}</h1>
+          <h3>{paintingData.artist.name}</h3>
+        </div>
       </div>
-      <div className={styles.artistInfo}>
-        <h1>{paintingData.name}</h1>
-        <h3>{paintingData.artist.name}</h3>
+
+      <div className={styles.rightContainer}>
+        <p>Lorem ipsum</p>
       </div>
+    
     </div>
   )
 }
