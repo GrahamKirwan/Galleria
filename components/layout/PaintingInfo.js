3a78d99359bd5ab516d4dbbc7c7f185dfa05ab59
Graham Kirwan <graham.kirwan4@gmail.com>
2022-06-19 21:12:13 -0400

    completed dynamic painting info page

--- components/layout/PaintingInfo.js
+++ components/layout/PaintingInfo.js
@@ -1,29 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
 import styles from '../layout/PaintingInfo.module.scss'
+import Link from 'next/link'
+
+import { TbArrowsMaximize } from "react-icons/tb";
+
 
 export default function PaintingInfo({paintingData}) {
 
-  console.log(paintingData)
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+  function viewImageClickHandler() {
+    setOverlayVisible(true)
+  }
+  
+  function overlayClickHandler() {
+    setOverlayVisible(false)
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <div style={{width: '30rem', height: '37rem', position: 'relative'}}>
+            <Image priority src={"/"+paintingData.images.gallery} layout='fill' objectFit='cover' />
+          </div>
+          <div className={styles.viewImage} onClick={viewImageClickHandler}>
+            <TbArrowsMaximize size="1.3rem" color="white" />
+            <span>View Image</span>
+          </div>
+          <div className={styles.artistInfo}>
+            <h1>{paintingData.name}</h1>
+            <h3>{paintingData.artist.name}</h3>
+          </div>
+          <div className={styles.artistImage}>
+            <div style={{width: '8rem', height: '8rem', position: 'relative'}}>
+              <Image src={"/"+paintingData.artist.image} layout='fill' objectFit='cover' alt={paintingData.artist.name} title={paintingData.artist.name} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightContainer}>
+          <span>{paintingData.year}</span>
+          <p>{paintingData.description}</p>
+          <Link href={paintingData.source}>
+            <a target="_blank">GO TO SOURCE</a>  
+          </Link>
+        </div>
+      </div>
 
-
-    
-      <div className={styles.leftContainer}>
+      <div className={styles.overlay} style={{display: overlayVisible ? 'block' : 'none'}} onClick={overlayClickHandler}>
         <div style={{width: '30rem', height: '37rem', position: 'relative'}}>
-          <Image src={"/"+paintingData.images.gallery} layout='fill' objectFit='cover' />
-        </div>
-        <div className={styles.artistInfo}>
-          <h1>{paintingData.name}</h1>
-          <h3>{paintingData.artist.name}</h3>
+          <Image priority src={"/"+paintingData.images.gallery} layout='fill' objectFit='cover' />
+          <p className={styles.close}>CLOSE</p>
         </div>
       </div>
-
-      <div className={styles.rightContainer}>
-        <p>Lorem ipsum</p>
-      </div>
-    
     </div>
   )
 }
